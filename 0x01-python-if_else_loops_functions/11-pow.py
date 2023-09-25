02455ee5b1eb11e49087202ce2098248b35c80e4
SmahEAli <samahelnourali@gmail.com>
2023-09-25 11:10:37 -0700

    first commit task 0-13 0x01-py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+#11-pow.py
+def pow(x, y):
+    return (x ** y)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#9-print_last_digit.py
+def print_last_digit(n):
+    print(abs(n) % 10, end="")
+    return (abs(n) % 10)
