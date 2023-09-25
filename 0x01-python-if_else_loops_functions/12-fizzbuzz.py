0ca602d6051eb16228d3fe3aac078b488f082ce0
SmahEAli <samahelnourali@gmail.com>
2023-09-25 13:26:46 -0700

    task8 2 prints 0x01-py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,7 @@
 # 12-fizzbuzz.py
 
 def fizzbuzz():
-    """print from 1 to 100 with space
-    
+    """print from 1 to 100 with space 
     For multiples of three print Fizz instead of the number
     and for multiples of five print Buzz
     For numbers which are multiples of both three and five print FizzBuzz
