6fd59778fe60fe1d0592489d555c6ee552f642e7
SmahEAli <samahelnourali@gmail.com>
2023-09-24 21:33:52 -0700

    tasks 4-10 0x00-py

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checker for cycle
+ * @list: checker
+ * Return: 1 if cycle or 0 if it no cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
