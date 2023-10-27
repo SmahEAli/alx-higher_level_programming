49c32e92334c63dd6841e049fbbe2bdce3944ea8
SmahEAli <samahelnourali@gmail.com>
2023-10-27 16:44:23 -0700

    Last commit T0-T8 of 0x07.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delim in ".?:":
+        # print(delim, text.split(delim))
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
