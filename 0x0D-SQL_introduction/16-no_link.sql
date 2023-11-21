09f5f562b51a3c00199ce93e1afaf0035a4b5427
SmahEAli <samahelnourali@gmail.com>
2023-11-20 18:52:31 -0800

    T0-16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- Lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 -- Records should be listed by descending score
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'name' != ""
-ORDER BY 'score' DESC;
+SELECT score, name
+FROM second_table
+WHERE name != ""
+ORDER BY score DESC;
